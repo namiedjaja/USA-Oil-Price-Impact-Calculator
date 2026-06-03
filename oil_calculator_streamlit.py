80098cd39d1863f1a62d1cace67b950776ab3cde
namiedjaja <namiedjaja@gmail.com>
2026-06-03 10:51:34 +0700

    Update oil_calculator_streamlit.py

--- oil_calculator_streamlit.py
+++ oil_calculator_streamlit.py
@@ -58,6 +58,6 @@ if st.button("Calculate changes", type="primary"):
         labels = ['Transportation', 'Food', 'Air Quality', 'Health']
     
         fig2, ax2 = plt.subplots()
-        ax2.pie(sizes, labels=labels, autopct='%1.1f%%',colors=custom_colors,textprops={'fontsize': 10})
+        ax2.pie(sizes, labels=labels, autopct='%1.1f%%',colors=custom_colors,textprops={'fontsize': 8})
         ax2.set_title('Share of Total Change by Sector')
         st.pyplot(fig2)
